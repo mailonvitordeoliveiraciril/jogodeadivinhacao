5877ce2064d743550125216e309fffc1c7140e96
mailonvitordeoliveiraciril <mailon.ciril@escola.pr.gov.br>
2025-07-31 12:17:55 +0000

    primeira parte

---
+++ jogo.py
@@ -0,0 +1,10 @@
+import random
+print("###########################")
+print("#    jogo de adivinhaçâo  #")
+print("#  @mailonvitordeoliveira #")
+print("###########################")
+print("seja bem vindo ao jogo  🙃 ")
+
+numeroSecreto= random.randrenge(0,100)
+totaltentativas = 0
+pontos = 100
